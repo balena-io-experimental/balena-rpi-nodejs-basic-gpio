10af09eeb456508b150aa604a197e41ff4afb593
Daniel Andrade <daniel@danielandrade.net>
2019-04-30 17:34:05 +0100

    Update balenalib

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+console.log('Starting');
+
 var  ledToggle, pressCount;
 //provision the gpio pins 22 for the led output and 17 for the button input
-var led= require("pi-pins").connect(22),
+var led = require("pi-pins").connect(22),
     button = require("pi-pins").connect(17);
 
 //set the pin mode,  setting pin 22 as an output and 17 as an input
 button.mode('in');
 led.mode('out');
 
-//set the initial value of the LED to be off.
+//set the initial value of the LED to be on.
 ledToggle = false;
 pressCount= 0;
 led.value(true);
